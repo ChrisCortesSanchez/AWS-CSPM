49e1148fc000d853b013e3acd964c68cdf80c582
ChrisCortesSanchez <cc7825@nyu.edu>
2026-03-18 20:38:44 -0400

    Added configurations for main.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# Which scanners to run
+ENABLED_SCANNERS = {
+    "s3": True,
+    "iam": True,
+    "ec2": True,
+    "cloudtrail": True,
+}
+
+# CIS AWS Benchmark version
+CIS_BENCHMARK_VERSION = "1.5.0"
+
+# Output directory
+OUTPUT_DIR = "./output"
